9cf31cde03eb2155bdbf347ade17a958dd036a9a
Neel Pawar <81655816+neelpawarcmu@users.noreply.github.com>
2022-12-27 19:45:39 -0500

    complete df functions for h1b scraper and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # hiring-data-scraper
+### levels fyi scraper
+(in progress)
+
+### h1b data scraper
+Scrapes data from [h1data.info](https://h1bdata.info/) into a dataframe, parses unique company names and populates a google sheet with the data
+To run this, follow the following
+1. clone this repo using git clone `https://github.com/neelpawarcmu/hiring-data-scraper.git`
+1. (optional) preferably install conda and create a conda env
+1. `conda install pip`
+2. install requirements using `pip
+3. follow [this tutorial]('test.com') to get a json file and add it to the `h1b-scraper` directory
+4. run `python h1b-scraper/h1b-scraper.py`
--- h1b-scraper/h1b-scraper.py
+++ h1b-scraper/h1b-scraper.py
@@ -1,20 +1,19 @@
 # Import libraries
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 from config import urls, companiesToExclude
+import gspread
+import df2gspread as d2g
 
 
 class Scraper:
-    def __init__(self, urls, table_id) -> None:
+    def __init__(self, urls) -> None:
         # Create an URL object
         self.urls = urls
-        self.table_id = table_id
     
     def scrape(self):
         self.generateDfFromURLs()
         self.processDf()
-        # print(self.df)
+        print(self.df)
 
     def generateDfFromURLs(self):
         '''
@@ -22,20 +21,48 @@ class Scraper:
         '''
         dfs = [pd.read_html(url)[0] for url in self.urls]
         self.df = pd.concat(dfs)
-        print(self.df)
+
+    def changeDfDtypes(self):
+        '''
+        change dtypes by mapping
+        '''
+        dtypes = {
+            'numeric_cols': ['BASE SALARY'],
+            'date_cols': []
+        }
+        numeric_cols = dtypes['numeric_cols']
+        self.df[numeric_cols] = self.df[
+            numeric_cols
+        ].apply(pd.to_numeric, errors='coerce', axis=1)
+
+    def cleanDf(self):
+        '''
+        drop extra columns and nan rows
+        drop blacklisted companies
+        '''
+        # drop 'Unnamed' cols from processing
+        self.df.drop(self.df.columns[self.df.columns.str.match("Unnamed")], axis=1, inplace=True)
+        # drop nan rows
+        self.df.dropna(axis=0, inplace=True)
+        # remove blacklisted companies 
+        self.df.drop(self.df[self.df.EMPLOYER.isin(companiesToExclude)].index, inplace=True)
 
     def processDf(self, order='BASE SALARY'):
         '''
         filter out and sort results
         '''
-        # remove blacklisted companies 
-        self.df.drop(self.df[self.df.EMPLOYER.isin(companiesToExclude)].index, inplace=True)
+        # change dtypes from str
+        self.changeDfDtypes()
+
+        # clean df nans and blacklisted companies
+        self.cleanDf()
+        
+        # change dtypes
         # group by company and order by salary
         self.df.sort_values('BASE SALARY', ascending=False, inplace=True)
         self.df.set_index([self.df['EMPLOYER'], self.df['BASE SALARY']], inplace=True)
         self.df.sort_index(level=0, inplace=True)
-        
 
 if __name__ == "__main__":
-    scraper = Scraper(urls, table_id="myTable")
+    scraper = Scraper(urls)
     scraper.scrape()
